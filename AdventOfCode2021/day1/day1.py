e0daa74150ca9ba1ee7ad609128e245f0454fcc4
msanchezzg <m.sanchezzgir@gmail.com>
2021-12-01 23:51:09 +0100

    AOC 2021 - day 1

--- AdventOfCode2021/day1/day1.py
+++ AdventOfCode2021/day1/day1.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
 import argparse
+
+
+def count_increments(listt):
+    num_increments = 0
+    for i in range(1, len(listt)):
+        if listt[i] > listt[i-1]:
+            num_increments += 1
+
+    return num_increments
 
 
 def main(input_file):
 
     with open(input_file, "r") as f:
-        lines = f.readlines()
+        numbers = [int(x) for x in f.read().split('\n')]
+
+    print('PART 1')
+    print(f'Number of increments = {count_increments(numbers)}')
+
+    print('\n-----------------------------\n')
+
+    numbers_group_3 = [numbers[i]+numbers[i+1]+numbers[i+2] for i in range(len(numbers)-2)]
+    print('PART 2')
+    print(f'Number of increments = {count_increments(numbers_group_3)}')
 
 
 if __name__ == "__main__":
